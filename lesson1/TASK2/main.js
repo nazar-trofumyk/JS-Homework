0f26b7f5ddd163ceea9b5effe6cbfff1b19efe11
nazar-trofumyk <gematogey@gmail.com>
2025-02-04 13:32:07 +0200

    add:main.js do:TASK2

---
+++ lesson1/TASK2/main.js
@@ -0,0 +1,6 @@
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let firstName="Nazar"
+let middleName=" Romanovych"
+let lastName=" Trofimuk"
+let msg=firstName + middleName + lastName
+console.log(msg)
