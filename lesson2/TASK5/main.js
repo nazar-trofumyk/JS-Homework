56e2f9ea2591ddbc0bba243e806ca486f18f1f01
nazar-trofumyk <gematogey@gmail.com>
2025-02-07 17:34:13 +0200

    add:main.js do:TASK2 do:TASK3 do:TASK4 add:lesson2 add:TASK1-2-3 add:TASK4 fixed:TASK add:node js for quo do:TASK4 do:TASK5 do:TASK1 do:TASK6,7,8,9,10

--- lesson2/TASK5/main.js
+++ lesson2/TASK5/main.js
@@ -3,12 +3,12 @@
 //     Нормальних варіантів опису - 2. Варіант, коли в вас буде одновимірний масив з 21 значенням вичключаємо одразу
 
 let weekWinter=[
-day1=[{inMorning:6},{inDay:15},{inEvening:12}],
-day2=[{inMorning:4},{inDay:6},{inEvening:2}],
-day3=[{inMorning:0},{inDay:4},{inEvening:-3}],
-day4=[{inMorning:-6},{inDay:-3},{inEvening:-8}],
-day5=[{inMorning:-3},{inDay:-1},{inEvening:-4}],
-day6=[{inMorning:1},{inDay:-2},{inEvening:-5}],
-    day7=[{inMorning:2},{inDay:-3},{inEvening:-6}],
+[ {Day:'Monday'},{inMorning:6},{inDay:15},{inEvening:12}],
+[ {Day:'Monday'},{inMorning:4},{inDay:6},{inEvening:2}],
+[ {Day:'Monday'},{inMorning:0},{inDay:4},{inEvening:-3}],
+[{Day:'Monday'}, {inMorning:-6},{inDay:-3},{inEvening:-8}],
+[ {Day:'Monday'},{inMorning:-3},{inDay:-1},{inEvening:-4}],
+[ {Day:'Monday'}, {inMorning:1},{inDay:-2},{inEvening:-5}],
+    [ {Day:'Monday'}, {inMorning:2},{inDay:-3},{inEvening:-6}],
 ]
 console.log(weekWinter)
