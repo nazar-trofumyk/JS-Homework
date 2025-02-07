887920ebac610cac103e504242715299aee0b277
nazar-trofumyk <gematogey@gmail.com>
2025-02-07 12:40:12 +0200

    add:main.js do:TASK2 do:TASK3 do:TASK4 add:lesson2 add:TASK1-2-3 add:TASK4 fixed:TASK add:node js for quo

--- lesson2/TASK4/main.js
+++ lesson2/TASK4/main.js
@@ -11,72 +11,72 @@
 // Василь Лисенко | vasyl_lys | VasyaCool777
 
 arr=[
-     users ={
-    user1:{
+    users= {
+        user1: {
 
-        name: 'Олег Сидорчук',
-        username: 'oleg_sid',
-        password: 'Oleg_7890',
-    },
-         user2:{
+            name: 'Олег Сидорчук',
+            username: 'oleg_sid',
+            password: 'Oleg_7890',
+        },
+        user2: {
 
-             name: 'Олег Сидорчук',
-             username: 'oleg_sid',
-             password: 'Oleg_7890',
-         },
-         user3:{
+            name: 'Олег Сидорчук',
+            username: 'oleg_sid',
+            password: 'Oleg_7890',
+        },
+        user3: {
 
-             name: 'Олег Сидорчук',
-             username: 'oleg_sid',
-             password: 'Oleg_7890',
-         },
-         user4:{
+            name: 'Олег Сидорчук',
+            username: 'oleg_sid',
+            password: 'Oleg_7890',
+        },
+        user4: {
 
-             name: 'Олег Сидорчук',
-             username: 'oleg_sid',
-             password: 'Oleg_7890',
-         },
-         user5:{
+            name: 'Олег Сидорчук',
+            username: 'oleg_sid',
+            password: 'Oleg_7890',
+        },
+        user5: {
 
-             name: 'Олег Сидорчук',
-             username: 'oleg_sid',
-             password: 'Oleg_7890',
-         },
-         user6:{
+            name: 'Олег Сидорчук',
+            username: 'oleg_sid',
+            password: 'Oleg_7890',
+        },
+        user6: {
 
-             name: 'Олег Сидорчук',
-             username: 'oleg_sid',
-             password: 'Oleg_7890',
-         },
-         user7:{
+            name: 'Олег Сидорчук',
+            username: 'oleg_sid',
+            password: 'Oleg_7890',
+        },
+        user7: {
 
-             name: 'Олег Сидорчук',
-             username: 'oleg_sid',
-             password: 'Oleg_7890',
-         },
-         user8:{
+            name: 'Олег Сидорчук',
+            username: 'oleg_sid',
+            password: 'Oleg_7890',
+        },
+        user8: {
 
-             name: 'Олег Сидорчук',
-             username: 'oleg_sid',
-             password: 'Oleg_7890',
-         },
-         user9:{
+            name: 'Олег Сидорчук',
+            username: 'oleg_sid',
+            password: 'Oleg_7890',
+        },
+        user9: {
 
-             name: 'Олег Сидорчук',
-             username: 'oleg_sid',
-             password: 'Oleg_7890',
-         },
-         user10:{
+            name: 'Олег Сидорчук',
+            username: 'oleg_sid',
+            password: 'Oleg_7890',
+        },
+        user10: {
 
-             name: 'Олег Сидорчук',
-             username: 'oleg_sid',
-             password: 'Oleg_7890',
-         },
+            name: 'Олег Сидорчук',
+            username: 'oleg_sid',
+            password: 'Oleg_7890',
+        },
 
+    }
 
-     }
      ]
-console.log(users.password)
-
+console.log(users. user1.password)
+console.log(users.user2.password)
 
 
