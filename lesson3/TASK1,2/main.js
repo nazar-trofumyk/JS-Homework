d5628491ac4a5b1d4ccf0d9032aada8278769a8f
nazar-trofumyk <gematogey@gmail.com>
2025-02-08 19:16:55 +0200

    add:main.js do:TASK2 do:TASK3 do:TASK4 add:lesson2 add:TASK1-2-3 add:TASK4 fixed:TASK add:node js for quo do:TASK4 do:TASK5 do:TASK1 do:TASK6,7,8,9,10 do:TASK 11,12 add:lesson3 do:task1,2,3,4

---
+++ lesson3/TASK1,2/main.js
@@ -0,0 +1,14 @@
+// #y7crMeFwHcS
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// #TYj7ncx
+
+for (let i=0;i<10;i++) {
+ document.write(`<div class="box"> Hi i'm studying Javascript  </div>`)
+}
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+//     #OeT7t3uUMFi
+
+for (let u =0;u<10;u++) {
+    document.write(`<div class="box-2"> ${u} Hello From Ukraine </div>`)
+
+}
