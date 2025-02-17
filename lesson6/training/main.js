0789c9e96ba4945e23bd14c98df4b6a729d4dfc6
nazar-trofumyk <gematogey@gmail.com>
2025-02-18 00:01:35 +0200

    add:main.js do:TASK2 do:TASK3 do:TASK4 add:lesson2 add:TASK1-2-3 add:TASK4 fixed:TASK add:node js for quo do:TASK4 do:TASK5 do:TASK1 do:TASK6,7,8,9,10 do:TASK 11,12 add:lesson3 do:task1,2,3,4 do:task5,6 do:task7,additional correct mistakes do:Lesson 4(TASK1,2,3,4,5,6) do:TASK 7,8,9,10,11,12,13 finish lesson 4 -start work with lesson 5 -do task 12,13 -finished lesson 5 -start lesson6 -do task 1,2,3

---
+++ lesson6/training/main.js
@@ -0,0 +1,27 @@
+// #dFeorS3m7u
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+
+let a = 'hello world'
+console.log(a.length)
+let b = 'Lorem ipsum'
+console.log(b.length)
+let c =  'javascript is not cool'
+console.log(c.length)
+// #8lld9HMxXWB
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+console.log(a.toUpperCase())
+console.log(b.toUpperCase())
+console.log(c.toUpperCase())
+
+// ClDsAm7xba7
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+let a1='HELLO WORLD'
+console.log(a1.toLowerCase())
+let b1='LOREM IPSUM'
+console.log(b1.toLowerCase())
+let c1 ='JAVASCRIPT IS NOT COOL'
+console.log(c1.toLowerCase())
---
+++ lesson6/training/training.js
@@ -0,0 +1,100 @@
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+//
+//
+// // users.forEach(function (value) {
+// //     console.log(value);
+// // });
+// //
+// // users.forEach(value => console.log(value));
+// //
+// // let filteredUsers = users.filter(value => value.age > 30);
+// // console.log(filteredUsers);
+// // console.log(users);
+// //
+// // let mapedUsers = users.map(function (value, index) {
+// //     return {...value, id: index + 1};
+// // });
+// // console.log(mapedUsers);
+// //
+// // users.map((value, index) => {
+// //     return {id: index + 1, name: value.name, age: value.age, status: value.status}
+// // })
+// //
+// //
+// // let find = users.find(value => value.name === 'max');
+// // console.log(find);
+// //
+// // console.log(users.every(value => value.status));
+// // console.log(users.some(value => value.status));
+//
+//
+// // let sort = users.sort((u1, u2) => {
+// //     return u2.age - u1.age;
+// // });
+// // console.log(sort);
+//
+// // console.log(users.sort((a, b) => {
+// //     if (a.name < b.name) {
+// //         return 1;
+// //     }
+// //     if (a.name > b.name) {
+// //         return -1
+// //     }
+// //     if (a.name === b.name) {
+// //         return 0
+// //     }
+// // }));
+//
+//
+// console.log(users.reduce((accumulator, user) => {
+//     if (user.status) {
+//         accumulator.statT.push(user);
+//     } else {
+//         accumulator.statF.push(user);
+//     }
+//     return accumulator;
+// }, {statT: [], statF: []}));
+//
+// console.log(users);
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+users.forEach(function (value){
+
+    console.log(value)
+
+
+})
+let filteredUsers = users.filter(value => value.age>30)
+console.log(filteredUsers)
+
+let mapedUsers = users.map(function (value,index){
+    return{...value,id:index + 1}
+})
+console.log(mapedUsers)
+
+let find = users.find(value =>value.status ===false)
+console.log(find)
+console.log(users.every(value => value.status))
