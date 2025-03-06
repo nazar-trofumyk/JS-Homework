0d6a4a61309a0699ae4a473f6ef003635812a0d6
nazar-trofumyk <gematogey@gmail.com>
2025-03-07 00:08:20 +0200

    add:main.js do:TASK2 do:TASK3 do:TASK4 add:lesson2 add:TASK1-2-3 add:TASK4 fixed:TASK add:node js for quo do:TASK4 do:TASK5 do:TASK1 do:TASK6,7,8,9,10 do:TASK 11,12 add:lesson3 do:task1,2,3,4 do:task5,6 do:task7,additional correct mistakes do:Lesson 4(TASK1,2,3,4,5,6) do:TASK 7,8,9,10,11,12,13 finish lesson 4 -start work with lesson 5 -do task 12,13 -finished lesson 5 -start lesson6 -do task 1,2,3,4,5,6,7,8,9 -just training -finished lesson6 -started lesson 7 -do task1,2,3,4,5 -will start task 6 -finished 7 task -do task with reduce -creat lesson8 d -do task 1 -do lesson 9 -training -training . -start work lesson 10 -do task - training localSTR and do hw -start lesson11 -do task 1 -start task2 -finish task2

--- lesson11/task2/main.js
+++ lesson11/task2/main.js
@@ -1,44 +1,56 @@
 const url = new URL('https://dummyjson.com/recipes');
 url.searchParams.set('limit', '10');
 
-let container = document.getElementsByClassName('container')[0];
+let box = document.getElementsByClassName('container')[0];
 fetch(url)
     .then(value => value.json())
     .then(recipesObject => {
-        const {recipes} = recipesObject
+ let {recipes} = recipesObject
+        console.log(recipes)
+
         for (const recipe of recipes) {
-            let box= document.createElement('div')
-            box.classList.add('target')
+            let container = document.createElement('div')
+            container.classList.add('containerRecipe')
+            box.appendChild(container)
+            for (const keyRecipe in recipe) {
 
-            for (const key in recipe) {
-                if (Array.isArray(recipe[key])) {
-                    let ul = document.createElement('ul')
-                    let button = document.createElement('button')
-                    button.innerText=`Show ${key}`
-ul.innerText = `${key}:`
+                if (Array.isArray(recipe[keyRecipe])){
+let divForList =document.createElement('div')
+                    divForList.classList.add('forList')
+                    let btn = document.createElement('button')
+                    btn.innerText = keyRecipe
+                    let list  = document.createElement('ul')
+                    // list.innerText = keyRecipe
+                    for (const item of recipe[keyRecipe]) {
+                       let li =document.createElement('li')
+                    li.innerText = item
+                        list.appendChild(li)
 
+                    }
+                    btn.onclick = function (){
+                        if (list.style.display ==='none'){
+                            list.style.display = 'block'
 
-                    const array = recipe[key]
-                    for (const arrayElement of array) {
-                        let li = document.createElement("li")
-                        li.innerText = `${arrayElement}`
-                        ul.appendChild(li)
+                        }else {
+                            list.style.display ='none'
+                        }
+
                     }
 
+                    divForList.appendChild(btn)
+                    divForList.appendChild(list)
+                    container.appendChild(divForList)
+                }else {  let target = document.createElement("div")
+                    target.innerText=`${keyRecipe}:${recipe[keyRecipe]}`
 
-                    box.appendChild(ul)
-                } else {
-                    let forItem = document.createElement('div')
-                    forItem.classList.add('item')
-                    box.appendChild(forItem)
-                    forItem.innerText = `${key} : ${recipe[key]}`
-                    box.appendChild(forItem)
-                }
+                    container.appendChild(target)}
+
 
             }
-            container.appendChild(box)
-        }
 
+
+
+        }
 
 
 
