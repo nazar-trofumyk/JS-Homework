887f18dca0c881dbb7d10ea1d818859063f74d80
nazar-trofumyk <gematogey@gmail.com>
2025-03-10 13:52:06 +0200

    add:main.js do:TASK2 do:TASK3 do:TASK4 add:lesson2 add:TASK1-2-3 add:TASK4 fixed:TASK add:node js for quo do:TASK4 do:TASK5 do:TASK1 do:TASK6,7,8,9,10 do:TASK 11,12 add:lesson3 do:task1,2,3,4 do:task5,6 do:task7,additional correct mistakes do:Lesson 4(TASK1,2,3,4,5,6) do:TASK 7,8,9,10,11,12,13 finish lesson 4 -start work with lesson 5 -do task 12,13 -finished lesson 5 -start lesson6 -do task 1,2,3,4,5,6,7,8,9 -just training -finished lesson6 -started lesson 7 -do task1,2,3,4,5 -will start task 6 -finished 7 task -do task with reduce -creat lesson8 d -do task 1 -do lesson 9 -training -training . -start work lesson 10 -do task - training localSTR and do hw -start lesson11 -do task 1 -start task2 -finish task2 -start control -do user-details.html

--- Control_JS/main.js
+++ Control_JS/main.js
@@ -7,13 +7,14 @@ let div =document.getElementsByClassName('container')[0]
           let boxUsers = document.createElement('div')
         boxUsers.classList.add('box_Users')
         let h1 = document.createElement('h1')
-        h1.innerText = `ID:${user.id}`
+        h1.innerText = `Id-${user.id}`
             let h2 = document.createElement('h2')
-           h2.innerText =`NAME:${user.name}`
+           h2.innerText =`Name-${user.name}`
 let btn = document.createElement('button')
            btn.classList.add('details')
             btn.innerText = 'Details Info'
             btn.onclick =function (){
+              location.href = `user-details.html?id=${user.id}`
 
             }
             boxUsers.append(h1,h2,btn)
---
+++ Control_JS/user_details.js
@@ -0,0 +1,35 @@
+const urlParams = new URLSearchParams(window.location.search);
+const userId = urlParams.get('id');
+let block = document.getElementsByClassName('block-for-user')[0]
+console.log(userId);
+fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    .then(response => response.json())
+    .then(user => {
+        console.log(user)
+        let wrapper = document.createElement('div' )
+        for (const userKey in user) {
+            let p = document.createElement('p')
+            let item = user[userKey]
+            if (typeof item ==='object' ){
+                let list =document.createElement('ul')
+                list.innerText=`${userKey}`
+                for (const key in item) {
+                    let li = document.createElement('li')
+                    if (typeof item[key] === 'object') {
+                        li.innerText = `${key}: { Lat: ${item[key].lat}, Lng: ${item[key].lng} }`;
+                    } else {
+                        li.innerText = `${key}:${item[key]}`;
+                    }
+                    list.appendChild(li)
+                }
+                wrapper.appendChild(p)
+                wrapper.appendChild(list)
+            }else { p.innerText =`${userKey}:${user[userKey]}`
+                wrapper.appendChild(p)}
+
+        }
+
+        wrapper.classList.add('wrapper_user')
+        block.appendChild(wrapper)
+
+    })
